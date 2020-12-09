6ef4a353ae5d5246f659d5c0cd39b7e7befab2e2
surbhi <surbhijain@immersiveinfotech.com>
2020-12-09 11:41:16 +0530

    design changes

--- purchase-receipt.php
+++ purchase-receipt.php
@@ -10,38 +10,47 @@
   .cancel-btn{padding:6px 12px;}
 </style>
 <!-- partial -->
+
+    
 <div class="row">
   <div class="col-lg-12">
     <div class="card mb-3">
       <div class="card-body">
+      <a href="add-retailer.php" type="button" class="btn btn-success btn-md mb-3 float-right"><i class="fa fa-plus" aria-hidden="true"></i> Add Purchase Receipt</a>
+
       <div class="table-responsive data-table">
-          <table class="table custom-table" id="example">
 
-            <thead class="Table-Header">
-            <tr>
-              <th rowspan="2" colspan="1">Product Code*...</th>
-              <th rowspan="2" colspan="1">Product Name</th>
-              <th rowspan="2" colspan="1">Batch*...</th>
-              <th rowspan="2" colspan="1">MRP</th>
-              <th rowspan="1" colspan="2">Order Qty</th>
-              <th rowspan="1" colspan="2">Invoice Qty</th>
-              <th rowspan="1" colspan="2">Rcvd. GOod Qty</th>
-              <th rowspan="2" colspan="2">Net Amount</th>
-            </tr>
+      <table class="table custom-table" id="example">
+        <thead class="Table-Header">
 
-          <tr>
-              <th>Uom</th>
-              <th>Qty</th>
-              <th>Uom</th>
-              <th>Qty</th>
-              <th>Uom</th>
-              <th>Qty</th>
-          </tr>
-      </thead>
+         <tr>
+            <th rowspan="2">Product Code*...</th>
+            <th rowspan="2">Product Name</th>
+            <th rowspan="2">Batch*...</th>
+            <th rowspan="2">MRP</th>
+            <th colspan="2" class="text-center">Order Qty</th>
+            <th colspan="2" class="text-center">Invoice Qty</th>
+            <th colspan="2" class="text-center">Rcvd. GOod Qty</th>
+            <th rowspan="2">Net Amount</th>
+         </tr>
+
+         <tr>
+
+          <th class="text-center">Uom</th>
+
+          <th class="text-center">Qty</th>
+          <th class="text-center">Uom</th>
+          <th class="text-center">Qty</th>
+          <th class="text-center">Uom</th>
+          <th class="text-center">Qty</th>
+
+         </tr>
+
+        </thead>
       
             <tbody>
               
-              <tr>
+               <tr>
                 <td>IN00001995</td>
                 <td>HINDUSTHAN MERCENTILE</td>
                 <td>1231</td>
@@ -54,7 +63,9 @@
                 <td>324.324</td>
                 <td>1022.23065448931</td>
               </tr>
-              <tr>
+
+              
+               <tr>
                 <td>IN00001995</td>
                 <td>HINDUSTHAN MERCENTILE</td>
                 <td>1231</td>
@@ -108,6 +119,7 @@
               </tr>
 
             </tbody>
+
           </table>
         </div>
       </div>
